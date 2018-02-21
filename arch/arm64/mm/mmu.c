c43cb87d95053ebf174e34cb14aa0866fcb9be1e
Will Deacon <will.deacon@arm.com>
2018-02-21 12:59:27 +0000

    arm64: Enforce BBM for huge IO/VMAP mappings
    commit 15122ee2c515a253b0c66a3e618bc7ebe35105eb upstream.
    
    ioremap_page_range doesn't honour break-before-make and attempts to put
    down huge mappings (using p*d_set_huge) over the top of pre-existing
    table entries. This leads to us leaking page table memory and also gives
    rise to TLB conflicts and spurious aborts, which have been seen in
    practice on Cortex-A75.
    
    Until this has been resolved, refuse to put block mappings when the
    existing entry is found to be present.
    
    Fixes: 324420bf91f60 ("arm64: add support for ioremap() block mappings")
    Reported-by: Hanjun Guo <hanjun.guo@linaro.org>
    Reported-by: Lei Li <lious.lilei@hisilicon.com>
    Acked-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Ben Hutchings <ben.hutchings@codethink.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/arm64/mm/mmu.c
+++ arch/arm64/mm/mmu.c
@@ -543,13 +543,10 @@ static void __init map_kernel(pgd_t *pgd)
 		 * entry instead.
 		 */
 		BUG_ON(!IS_ENABLED(CONFIG_ARM64_16K_PAGES));
-<<<<<<< HEAD
 		set_pud(pud_set_fixmap_offset(pgd, FIXADDR_START),
 			__pud(__pa_symbol(bm_pmd) | PUD_TYPE_TABLE));
-=======
 		pud_populate(&init_mm, pud_set_fixmap_offset(pgd, FIXADDR_START),
 			     lm_alias(bm_pmd));
->>>>>>> 17c413903026 (arm64: don't open code page table entry creation)
 		pud_clear_fixmap();
 	} else {
 		BUG();
@@ -866,11 +863,17 @@ int pud_set_huge(pud_t *pudp, phys_addr_t phys, pgprot_t prot)
 	pgprot_t sect_prot = __pgprot(PUD_TYPE_SECT |
 					pgprot_val(mk_sect_prot(prot)));
 <<<<<<< HEAD
+<<<<<<< HEAD
 	pud_t new_pud = pfn_pud(__phys_to_pfn(phys), sect_prot);
 
 	/* Only allow permission changes for now */
 	if (!pgattr_change_is_safe(READ_ONCE(pud_val(*pud)),
 				   pud_val(new_pud)))
+=======
+
+	/* ioremap_page_range doesn't honour BBM */
+	if (pud_present(READ_ONCE(*pudp)))
+>>>>>>> 4f45a0a17035 (arm64: Enforce BBM for huge IO/VMAP mappings)
 		return 0;
 
 	BUG_ON(phys & ~PUD_MASK);
@@ -891,11 +894,17 @@ int pmd_set_huge(pmd_t *pmdp, phys_addr_t phys, pgprot_t prot)
 	pgprot_t sect_prot = __pgprot(PMD_TYPE_SECT |
 					pgprot_val(mk_sect_prot(prot)));
 <<<<<<< HEAD
+<<<<<<< HEAD
 	pmd_t new_pmd = pfn_pmd(__phys_to_pfn(phys), sect_prot);
 
 	/* Only allow permission changes for now */
 	if (!pgattr_change_is_safe(READ_ONCE(pmd_val(*pmd)),
 				   pmd_val(new_pmd)))
+=======
+
+	/* ioremap_page_range doesn't honour BBM */
+	if (pmd_present(READ_ONCE(*pmdp)))
+>>>>>>> 4f45a0a17035 (arm64: Enforce BBM for huge IO/VMAP mappings)
 		return 0;
 
 	BUG_ON(phys & ~PMD_MASK);
