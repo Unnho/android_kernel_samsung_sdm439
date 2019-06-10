4240de2222676616a384e82842fb4b5e2c32f66e
Jorge Ramirez-Ortiz <jorge.ramirez-ortiz@linaro.org>
2019-06-10 19:23:08 +0200

    tty: serial: msm_serial: avoid system lockup condition
    [ Upstream commit ba3684f99f1b25d2a30b6956d02d339d7acb9799 ]
    
    The function msm_wait_for_xmitr can be taken with interrupts
    disabled. In order to avoid a potential system lockup - demonstrated
    under stress testing conditions on SoC QCS404/5 - make sure we wait
    for a bounded amount of time.
    
    Tested on SoC QCS404.
    
    Signed-off-by: Jorge Ramirez-Ortiz <jorge.ramirez-ortiz@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/tty/serial/msm_serial.c
+++ drivers/tty/serial/msm_serial.c
@@ -398,6 +398,7 @@ no_rx:
 static inline void msm_wait_for_xmitr(struct uart_port *port)
 {
 	u32 count = 500000;
+	unsigned int timeout = 500000;
 
 	while (!(msm_read(port, UART_SR) & UART_SR_TX_EMPTY)) {
 		if (msm_read(port, UART_ISR) & UART_ISR_TX_READY)
@@ -413,6 +414,8 @@ static inline void msm_wait_for_xmitr(struct uart_port *port)
 			printk_deferred("uart may lost data, resetting TX!\n");
 			break;
 		}
+		if (!timeout--)
+			break;
 	}
 	msm_write(port, UART_CR_CMD_RESET_TX_READY, UART_CR);
 }
