59fbdfef75a8a05901c129d8445f49ada765c8c8
Suren Baghdasaryan <surenb@google.com>
2020-01-27 15:56:16 -0800

    staging: android: ashmem: Disallow ashmem memory from being remapped
    commit 6d67b0290b4b84c477e6a2fc6e005e174d3c7786 upstream.
    
    When ashmem file is mmapped, the resulting vma->vm_file points to the
    backing shmem file with the generic fops that do not check ashmem
    permissions like fops of ashmem do. If an mremap is done on the ashmem
    region, then the permission checks will be skipped. Fix that by disallowing
    mapping operation on the backing shmem file.
    
    Reported-by: Jann Horn <jannh@google.com>
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    Cc: stable <stable@vger.kernel.org> # 4.4,4.9,4.14,4.18,5.4
    Signed-off-by: Todd Kjos <tkjos@google.com>
    Reviewed-by: Joel Fernandes (Google) <joel@joelfernandes.org>
    Link: https://lore.kernel.org/r/20200127235616.48920-1-tkjos@google.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/android/ashmem.c
+++ drivers/staging/android/ashmem.c
@@ -380,6 +380,8 @@ static unsigned long
 ashmem_vmfile_get_unmapped_area(struct file *file, unsigned long addr,
 			unsigned long len, unsigned long pgoff,
 			unsigned long flags)
+				unsigned long len, unsigned long pgoff,
+				unsigned long flags)
 {
 	return current->mm->get_unmapped_area(file, addr, len, pgoff, flags);
 }
@@ -438,6 +440,7 @@ static int ashmem_mmap(struct file *file, struct vm_area_struct *vma)
 			vmfile_fops.mmap = ashmem_vmfile_mmap;
 			vmfile_fops.get_unmapped_area =
 				ashmem_vmfile_get_unmapped_area;
+					ashmem_vmfile_get_unmapped_area;
 		}
 		vmfile->f_op = &vmfile_fops;
 	}
